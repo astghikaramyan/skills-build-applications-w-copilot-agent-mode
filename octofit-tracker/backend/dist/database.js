416c1ad1bdefbd7ee4a7245d99b3f6d1224fa4f5
astghikaramyan <53055716+astghikaramyan@users.noreply.github.com>
2026-07-01 05:53:54 +0000

    Fix backend database module exports

--- octofit-tracker/backend/dist/database.js
+++ octofit-tracker/backend/dist/database.js
@@ -3,16 +3,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.database = exports.mongoUri = void 0;
+exports.mongoose = exports.mongoUri = void 0;
 exports.connectToDatabase = connectToDatabase;
 const mongoose_1 = __importDefault(require("mongoose"));
+exports.mongoose = mongoose_1.default;
 exports.mongoUri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/octofit_db';
-exports.database = mongoose_1.default;
 async function connectToDatabase() {
-    if (exports.database.connection.readyState >= 1) {
-        return exports.database.connection;
+    if (mongoose_1.default.connection.readyState >= 1) {
+        return mongoose_1.default.connection;
     }
-    await exports.database.connect(exports.mongoUri);
-    return exports.database.connection;
+    await mongoose_1.default.connect(exports.mongoUri);
+    return mongoose_1.default.connection;
 }
-exports.default = exports.database;
+exports.default = mongoose_1.default;
